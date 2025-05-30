37657ecf8f019ab0d3e5879b105e91a1f1d9cda2
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2025-05-30 19:49:54 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 67892 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int arr[];
+    static int n; static int m;
+
+    static void dfs(int index){
+        if (index == m){
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            arr[index] = i;
+            dfs(index + 1);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m+1];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+}
