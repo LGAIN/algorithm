c4f16cb479c17670887bdc6988dae36ba75f0764
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2025-09-25 18:06:09 +0900

    [Silver III] Title: N번째 큰 수, Time: 904 ms, Memory: 243668 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+
+                if (pq.size() > n) {
+                    pq.poll();
+                }
+            }
+        }
+
+        System.out.println(pq.peek());
+    }
+}
