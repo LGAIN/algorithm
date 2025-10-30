0f3be97247145ca3e755017739c2ea2485518a9b
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2025-10-30 14:26:51 +0900

    [level 2] Title: 타겟 넘버, Time: 7.29 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,20 @@
 class Solution {
+    
+    static int answer = 0;
+    
+    public void dfs(int[] numbers, int target, int sum, int depth) {
+        if (depth == numbers.length) {
+            if (sum == target) answer++;
+            return;
+        }
+        dfs(numbers, target, sum - numbers[depth], depth + 1);
+        dfs(numbers, target, sum + numbers[depth], depth + 1);
+    }
+    
+    
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        
+        dfs(numbers, target, 0, 0);
         return answer;
     }
 }
