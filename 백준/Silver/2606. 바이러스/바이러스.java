d4f378ae4cef94fbb790a7fd719ee073de011c89
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2025-11-05 16:45:51 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,39 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int computer;
-    static boolean[] check;
-    static int[][] map;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[][] map;
+	static boolean[] visited;
+	static int count;
 
+	public static void dfs(int i) {
+		visited[i] = true;
+		for (int k = 1; k < map.length; k++) {
+			if (map[i][k] == 1 && !visited[k]) {
+				count++;
+				dfs(k);
+			}
+		}
+	}
 
-    public static void main(String[] args) throws IOException{
-        computer = Integer.parseInt(br.readLine());
-        int connection = Integer.parseInt(br.readLine());
-        map = new int[computer+1][computer+1];
-        check = new boolean[computer+1];
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		int v = Integer.parseInt(br.readLine());
+		map = new int[n + 1][n + 1];
+		visited = new boolean[n + 1];
 
-        for (int i = 1; i <= connection; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < v; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			map[a][b] = map[b][a] = 1;
+		}
 
-            map[a][b] = map[b][a] = 1;
-        } // for
+		count = 0;
+		dfs(1);
 
-        System.out.println(dfs(1, 0));
-
-    } // main
-
-    public static int dfs(int start, int cnt) {
-        check[start] = true;
-        for (int i = 1; i <= computer; i++) {
-            if (map[start][i] == 1 && !check[i]) {
-                cnt = dfs(i, cnt+1);
-            }
-        } // for
-        return cnt;
-    } // dfs
-
+		System.out.println(count);
+	}
 }
