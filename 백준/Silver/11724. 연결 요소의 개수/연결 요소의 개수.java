02b2e6a300ff6db73cbe12a04a5042d228ea8bda
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2025-11-06 16:19:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 488 ms, Memory: 117220 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,49 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static ArrayList<Integer>[] graph;
-    static boolean[] visit;
-    static int n;
+	static BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[][] map;
+	static boolean[] visited;
+	static int n;
+	static int m;
+	static int count;
 
-    static void dfs(int start) {
-        visit[start] = true;
+	public static void dfs(int i) {
+		visited[i] = true;
+		for (int j = 1; j <= n; j++) {
+			if (map[i][j] == 1 && !visited[j]) {
+				dfs(j);
+			}
+		}
+	}
 
-        for (int next : graph[start]) {
-            if (!visit[next]) {
-                dfs(next);
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n + 1][n + 1];
+		visited = new boolean[n + 1];
 
-    } // dfs
+		for (int i = 1; i <= m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			map[a][b] = map[b][a] = 1;
+		}
 
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[n+1];
-        visit = new boolean[n+1];
-        int cnt = 0;
-        // v 입력
-        for (int v = 1; v <= n; v++) {
-            graph[v] = new ArrayList<>();
-        }
-        // e 입력
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
-        }
 
-        for (int i = 1; i <= n; i++) {
-            if (!visit[i]) {
-                dfs(i);
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
-    } // main
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) {
+				dfs(i);
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
 }
