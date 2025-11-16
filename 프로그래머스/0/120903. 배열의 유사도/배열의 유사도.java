64208c9f95e52d02f78349bef5a776228af4cc11
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2025-11-16 13:24:05 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for (String word1 : s1) {
+            for (String word2 : s2) {
+                if (word1.equals(word2)) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
