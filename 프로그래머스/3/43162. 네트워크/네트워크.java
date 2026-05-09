84c8ce4654cc3671a616ae10712616e4a10d9a3e
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2026-05-09 16:25:54 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,77 @@
+import java.util.*;
+
 class Solution {
-    static boolean[] visited;
     
-    public void dfs(int i, int[][] computers, int n) {
-        visited[i] = true;
-        
-        for (int j = 0; j < n; j++) {
-            if (j != i && !visited[j] && computers[i][j] == 1) {
-                dfs(j, computers, n);
+    boolean[] visited;
+    
+    public void dfs(int i, int[][] computers) {
+        for (int a = 0; a < computers.length; a++) {
+            if (i != a && computers[i][a] == 1 && !visited[a]) {
+                visited[a] = true;
+                dfs(a, computers);
             }
         }
     }
-    
     
     public int solution(int n, int[][] computers) {
-        int count = 0;
         visited = new boolean[n];
+        int answer = 0;
+        
         for (int i = 0; i < n; i++) {
-            if (!visited[i]){
-                dfs(i, computers, n);
-                count++;
+            if (!visited[i]) {
+                dfs(i, computers);
+                answer++;
             }
         }
-        return count;
-    }
-}
+        
+        return answer;
+     } // solution
+} // Solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+    
+//     static boolean[] visited;
+    
+//     public static void dfs(int x, int n, int[][] computers) {
+//         visited[x] = true;
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i] && computers[x][i] == 1) {
+//                 visited[i] = true;
+//                 dfs(i, n, computers);
+//             }
+//         }
+//     }
+    
+    
+//     public int solution(int n, int[][] computers) {
+//         int answer = 0;
+//         visited = new boolean[n];
+        
+//         for (int i = 0; i < n; i++) { 
+//             for (int j = 0; j < n; j++) {
+//                 if (!visited[i] && computers[i][j] == 1) {
+//                     dfs(i, n, computers);
+//                     answer++;
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
