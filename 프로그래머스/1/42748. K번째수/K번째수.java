a4de3992af0bdf90f55b5c0b528e71d73f2d34e5
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2026-05-12 14:37:13 +0900

    [level 1] Title: K번째수, Time: 0.29 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,18 +2,49 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        int[] answer = new int [commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0] - 1;
-            int end = commands[i][1];
-            int k = commands[i][2] - 1;
-            
-            // 배열 추출
-            int[] temp = Arrays.copyOfRange(array, start, end);
+            int length = commands[i][1] - commands[i][0] + 1;
+            int[] temp = new int[length];
+            int idx = 0;
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                temp[idx] = array[j];
+                idx++;
+            }
             Arrays.sort(temp);
-            answer[i] = temp[k];
+            answer[i] = temp[commands[i][2] - 1];
+            
         }
+        
         return answer;
-    }
-}
+    } // solution
+} // Solution
+
+
+
+
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] array, int[][] commands) {
+//         int[] answer = new int[commands.length];
+        
+//         for (int i = 0; i < commands.length; i++) {
+//             int start = commands[i][0] - 1;
+//             int end = commands[i][1];
+//             int k = commands[i][2] - 1;
+            
+//             // 배열 추출
+//             int[] temp = Arrays.copyOfRange(array, start, end);
+//             Arrays.sort(temp);
+//             answer[i] = temp[k];
+//         }
+//         return answer;
+//     }
+// }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 86.2 MB, 시간: 0.35 ms
+메모리: 61.3 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 29일 17:22:04
+2026년 05월 12일 14:37:09
 
 ### 문제 설명
 
