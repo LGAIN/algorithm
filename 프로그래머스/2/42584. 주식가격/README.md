37256e77251f5221701fd2dcfaaed8a15ed87aa0
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2026-05-12 17:10:21 +0900

    [level 2] Title: 주식가격, Time: 14.87 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 7.55 ms
+메모리: 81.1 MB, 시간: 14.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 12일 16:20:29
+2026년 05월 12일 17:10:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,52 +1,93 @@
-class Solution {
-    public int[] solution(int[] prices) {
-
-        int[] answer = new int[prices.length];
-
-        for (int j = 0; j < prices.length; j++) {
-
-            int count = 0;
-
-            for (int i = j + 1; i < prices.length; i++) {
-
-                count++;
-
-                if (prices[j] > prices[i]) {
-                    break;
-                }
-            }
-
-            answer[j] = count;
-        }
-
-        return answer;
-    }
-}
-
 // import java.util.*;
 
 // class Solution {
 //     public int[] solution(int[] prices) {
-//         int n = prices.length;
-//         int[] answer = new int[n];
-        
+//         int[] answer = new int[prices.length];
 //         Stack<Integer> stack = new Stack<>();
-//         stack.push(0);
         
-//         for (int i = 1; i < n; i++) {
-//             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-//                 // 현재 가격이 이전 가격보다 하락했다면
-//                 int j = stack.pop();
-//                 answer[j] = i - j;
+//         for (int i = 0; i < prices.length; i++) {
+//             while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+//                 int idx = stack.pop();
+//                 answer[idx] = i - idx;
 //             } // while
+            
 //             stack.push(i);
+                
 //         } // for
         
 //         while (!stack.isEmpty()) {
-//             int j = stack.pop();
-//             answer[j] = n - 1 - j;
-//         }
+//             int idx = stack.pop();
+//             answer[idx] = prices.length - idx - 1;
+//         } // while
         
+//         return answer;
+//     } // solution
+// } // Solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] solution(int[] prices) {
+
+//         int[] answer = new int[prices.length];
+
+//         for (int j = 0; j < prices.length; j++) {
+
+//             int count = 0;
+
+//             for (int i = j + 1; i < prices.length; i++) {
+
+//                 count++;
+
+//                 if (prices[j] > prices[i]) {
+//                     break;
+//                 }
+//             }
+
+//             answer[j] = count;
+//         }
+
 //         return answer;
 //     }
 // }
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                // 현재 가격이 이전 가격보다 하락했다면
+                int j = stack.pop();
+                answer[j] = i - j;
+            } // while
+            stack.push(i);
+        } // for
+        
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            answer[j] = n - 1 - j;
+        }
+        int a = 1;
+        return answer;
+    }
+}
