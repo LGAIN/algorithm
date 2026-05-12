e682ef26c4f32c3290390a2b1545e666fc633107
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2026-05-12 16:20:31 +0900

    [level 2] Title: 주식가격, Time: 7.55 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,52 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-        
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        
-        for (int i = 1; i < n; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                // 현재 가격이 이전 가격보다 하락했다면
-                int j = stack.pop();
-                answer[j] = i - j;
-            } // while
-            stack.push(i);
-        } // for
-        
-        while (!stack.isEmpty()) {
-            int j = stack.pop();
-            answer[j] = n - 1 - j;
+
+        int[] answer = new int[prices.length];
+
+        for (int j = 0; j < prices.length; j++) {
+
+            int count = 0;
+
+            for (int i = j + 1; i < prices.length; i++) {
+
+                count++;
+
+                if (prices[j] > prices[i]) {
+                    break;
+                }
+            }
+
+            answer[j] = count;
         }
-        
+
         return answer;
     }
 }
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] prices) {
+//         int n = prices.length;
+//         int[] answer = new int[n];
+        
+//         Stack<Integer> stack = new Stack<>();
+//         stack.push(0);
+        
+//         for (int i = 1; i < n; i++) {
+//             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+//                 // 현재 가격이 이전 가격보다 하락했다면
+//                 int j = stack.pop();
+//                 answer[j] = i - j;
+//             } // while
+//             stack.push(i);
+//         } // for
+        
+//         while (!stack.isEmpty()) {
+//             int j = stack.pop();
+//             answer[j] = n - 1 - j;
+//         }
+        
+//         return answer;
+//     }
+// }
